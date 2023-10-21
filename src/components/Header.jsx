d48b1ce1392bdd9df9976e20f2e47e1d6a430da4
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-10-21 13:58:28 -0400

    Adjusting Footer, Header, Navbar and About page

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+// This will be on top of the Navbar
+
+const styles = {
+    footer: {
+      margin: 20,
+      background: '#e8eaf6',
+    },
+    heading: {
+      background: '#3f51b5',
+      minHeight: 40,
+      lineHeight: 3.5,
+      fontSize: '1.2rem',
+      color: 'white',
+      padding: '0 20px',
+    },
+    content: {
+      padding: 20,
+    },
+  };
+
+function Footer() {
+    return (
+      <div style={styles.footer}>
+        <div style={styles.heading}>More about Lyell</div>
+        <div style={styles.content}>
+          `On the front-end, Lyell is a master of user interface (UI) design and development. He creates visually appealing and user-friendly websites that engage and captivate visitors. His proficiency in HTML, CSS, and JavaScript allows him to craft responsive and interactive web interfaces `
+        </div>
+      </div>
+    );
+  }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,12 +5,12 @@ export default function About() {
       <br></br>
       <h2>Lyell Curtis</h2>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      Full stack web developer entering from a graphic design background. My goal is to put a creative input for not only UX/UI but also solutions to user problems. Earned a certificate in Full Stack Web Development from the Edx program at the University of Pennsylvania. Fast learner and adaptor of maintaining dynamic SEO websites from the user perspective. Focuses are creative solutions, project planning, and building innovative project ideas.
+
+      Laerned the following: 
+      JavaScript, CSS3, HTML5, GitHub, MySQL, Express, React, Node, Handlebars, jQuery, Bootstrap, OOP, Sequelize.js 
+
+      In addition to his technical skills, Lyell Curtis is known for his problem-solving abilities, teamwork, and communication skills. He works collaboratively with cross-functional teams to turn project requirements into functional web solutions. Lyell is committed to staying updated with the latest web development trends and technologies, allowing him to provide cutting-edge solutions to clients and businesses.
       </p>
     </div>
   );
