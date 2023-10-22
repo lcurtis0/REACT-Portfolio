2574a8d99834f49ec87b729bed7889a6df95f95e
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-10-22 14:20:19 -0400

    contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,90 @@
 
 
 
+import { useState } from 'react';
 
-
-export default function About() {
+export default function Contact() {
   return (
     <div>
       <h1>About Me</h1>
       <br></br>
-      <h2>Lyell Curtis</h2>
-      {/*<img src="./assets/images/Avatar(v2).jpg" alt="Avatar of Lyell Curtis" class="Avatar"></img> */}
-      <p>Lyell M. Curtis</p>
-      <a href="https://github.com/lcurtis0">
-        <p class="tag">- My Github: </p>
-      </a>
-      <p class="tag"> - lmcurtis222@gmail.com </p>
-      <label>- 555-555-5555 </label>
-      <label> - 8:00am - 5:00pm</label>
+      <h2>Contact Lyell Curtis</h2>
+
+      {/* form with fields for a name, an email address, and a message */}
+
+      const [firstName, setFirstName] = useState('');
+
+      const [lastName, setLastName] = useState('');
+
+      const [email, setEmail] = useState('');
+
+      const [message, setMessage] = useState('');
+
     </div>
   );
 }
+
+const inputChange = (e) => {
+
+  const { name, value } = e.target;
+  const email = e.target;
+
+  return name === 'firstName' ? setFirstName(value) : setLastName(value);
+};
+
+const handleFormSubmit = (e) => {
+
+  e.preventDefault();
+
+  alert(`Hello ${firstName} ${lastName} your message was sent by you email ${email}`);
+  setFirstName('');
+  setLastName('');
+  setEmail('');
+  setMessage('');
+};
+
+return (
+  <div className="container text-center">
+    <h1>
+      Hello {firstName} {lastName}
+    </h1>
+    <form className="form" onSubmit={handleFormSubmit}>
+
+      <input
+        value={firstName}
+        name="firstName"
+        onChange={inputChange}
+        type="text"
+        placeholder="First Name"
+      />
+      <input
+        value={lastName}
+        name="lastName"
+        onChange={inputChange}
+        type="text"
+        placeholder="Last Name"
+      />
+
+      <input
+        value={email}
+        name="email"
+        onChange={inputChange}
+        type="text"
+        placeholder="Email"
+      />
+
+      <input
+        value={message}
+        name="message"
+        onChange={inputChange}
+        type="text"
+        placeholder="Message"
+      />
+
+      <button type="submit">
+        Submit Message
+      </button>
+    </form>
+  </div>
+);
+
