986264f97cb4451bb3123722430d9172cca0703c
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-10-23 17:02:32 -0400

    Finished Porfolio page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,6 +3,7 @@ function Resume() {
     return (
         <ul className="list-group">
             <h1> My Reume</h1>
+            <hr></hr>
             <li>
                 <a href="./Assets/images/Resume of Lyell Curtis (2).pdf">
                     <img
