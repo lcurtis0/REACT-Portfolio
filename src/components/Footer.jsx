f48564584333db971307356ca9191761c1a419e3
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-10-23 13:45:51 -0400

    Added stackover flow

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,7 +29,20 @@ function Footer() {
               src="Assets/images/Github logo (png).png"
             />
           </a>
+
           <p>Github: <a href="https://github.com/lcurtis0">  lcurtis0/ </a> </p>
+          </li>
+
+          <li>
+          <a href="https://stackoverflow.com/users/22790359/lyell-curtis">
+            <img
+              alt="stack overflow logo"
+              className="logo"
+              src="Assets/images/stack overflow logo.png"
+            />
+          </a>
+          
+          <p>Stack Overflow: <a href="https://stackoverflow.com/users/22790359/lyell-curtis">  lcurtis0/ </a> </p>
         </li>
       </ul>
       <div >
